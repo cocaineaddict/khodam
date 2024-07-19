250a2e23861579ac4f7b2b5387a338c9a4b8e795
Jeffrey Young <jeffreyyoungpersonal@gmail.com>
2024-07-20 01:31:25 +0700

    khodam init

---
+++ khodam.py
@@ -0,0 +1,58 @@
+import warnings
+
+warnings.filterwarnings("ignore", message="Signature .* for <class 'numpy.longdouble'> does not match any known type")
+
+import numpy as np
+import pandas as pd
+
+animals_50 = [
+    "Harimau", "Komodo", "Orang Utan", "Anjing Kintamani", "Merak",
+    "Badak Bercula Satu", "Garuda", "Macan Dahan", "Banteng", "Ayam Bekisar",
+    "Jelarang", "Lumba-lumba", "Cenderawasih", "Tapir", "Elang Jawa",
+    "Kucing Sumatera", "Naga Air", "Barong", "Rangda", "Legong",
+    "Anoa", "Cendrawasih", "Badak Sumatera", "Gajah Sumatra", "Macan Tutul Jawa",
+    "Penyu Belimbing", "Buaya Muara", "Bekantan", "Rusa Timor", "Kalong",
+    "Paus Sperma", "Hirola", "Tarsius", "Ular Sanca Kembang", "Ajag",
+    "Burung Mandar", "Elang Flores", "Kakatua Raja", "Kuskus", "Monyet Hitam Sulawesi",
+    "Kadal Draco", "Kanguru Pohon", "Ikan Arwana", "Ikan Cupang", "Kuda Nil",
+    "Gajah Borneo", "Landak Jawa", "Tupai Terbang", "Katak Pohon", "Ular Piton"
+]
+
+powers_50 = [
+    "Melompat", "Menyala", "Mengambang", "Jentikan", "Gelap",
+    "Terangi", "Kilatan", "Lenggok", "Silaukan", "Gemerlap",
+    "Bergetar", "Pijarkan", "Terawang", "Bertukar", "Menyatu",
+    "Berubah", "Menggulung", "Berputar", "Pecah", "Goyang",
+    "Bertahan", "Gesit", "Bringas", "Ngelmu", "Mendung",
+    "Berkilau", "Bercahaya", "Berkelebat", "Melayang", "Merayap",
+    "Membesar", "Mengecil", "Terbang", "Lari", "Ngedrip",
+    "Nglayang", "Nggeblak", "Bledosan", "Nglimpe", "Nglecer",
+    "Ceprotan", "Lemes", "Cemeng", "Nyenyet", "Kebak",
+    "Nglebur", "Ngemot", "Gumulung", "Berbunyi", "Petir"
+]
+
+df_50 = pd.DataFrame({
+    "Animals": animals_50,
+    "Powers": powers_50
+})
+
+csv_path_50 = "/home/cocaine/khodam/Animals_and_Powers_50.csv"
+df_50.to_csv(csv_path_50, index=False)
+
+def load_data(filepath):
+    df = pd.read_csv(filepath)
+    return df['Animals'].tolist(), df['Powers'].tolist()
+
+def calculate_khodamcode(input_string):
+    return sum(ord(char) for char in input_string)
+
+def select_animal_and_power(animals, powers, code):
+    index = code % 50
+    return animals[index], powers[index]
+
+if __name__ == "__main__":
+    animals, powers = load_data(csv_path_50)
+    user_input = input("Cek Khodam! tulislah nama anda: ")
+    khodamcode = calculate_khodamcode(user_input)
+    animal, power = select_animal_and_power(animals, powers, khodamcode)
+    print(f"{animal} {power}")
